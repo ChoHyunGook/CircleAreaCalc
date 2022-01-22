09acdcc8093a74f9bfd1942e8021c17985fe8c79
Daniel Cho Codes <whgusrnr7755@gmail.com>
2022-01-23 01:17:10 +0900

    2022-01-23 01:16 반지름입력하면 원넓이가 구해지는 프로그램

---
+++ java/com/circlearea/web/CircleAreaCalcApplication.java
@@ -0,0 +1,13 @@
+package com.circlearea.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleAreaCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleAreaCalcApplication.class, args);
+    }
+
+}
